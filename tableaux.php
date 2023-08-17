08671bd6716c3a668fa7821eaf744bbd42127660
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-17 18:48:34 +0200

    created tab

---
+++ tableaux.php
@@ -0,0 +1,3 @@
+<?php
+$notes = [17, 19];
+echo $notes[1];
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+$nom = 'Thomas';
+
+$note = 17;
+$note2 = 19;
+$moyenne = ($note + $note2) /2;
+echo 'Bonjour '.$nom.' vous avez eu '.$moyenne. ' de moyenne'."\n";
+echo "Bonjour $nom vous avez eu $moyenne de moyenne";
+
+//Autres types de variables communément utilisés true, false, null, array, object
+
+?>
