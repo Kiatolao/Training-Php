23d7939fa84a2aa1c6dfb9ce50a86a714dc9e7da
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-17 20:14:54 +0200

    Update tableaux.php

--- tableaux.php
+++ tableaux.php
@@ -1,3 +1,33 @@
 <?php
-$notes = [17, 19];
-echo $notes[1];
+//Tableaux simple
+$notes = [17, 19, 15, 18, 14, 16];
+echo "Tableau simple $notes[3]\n";
+
+//Tableaux multiple
+$eleves = ['Thomas', 'Kia', [17, 19, 15, 18, 14, 16]];
+echo 'Tableau multiple '. $eleves[2][4]."\n";
+
+//Tableaux multiple associatif
+$eleves = ['prenom' => 'Thomas',
+            'nom' => 'Kia',
+            'notes' => [17, 19, 15, 18, 14, 16]
+        ];
+
+//Ajout d'une note
+$eleves['notes'][] = 20;
+echo 'Tableau multiple particulier '. $eleves['nom']. ' '.$eleves['prenom']."\n";
+print_r($eleves['notes']);
+
+//Tableaux multidimensionnel
+$classe = [
+    [
+        'prenom' => 'Thomas',
+        'nom' => 'Kia',
+        'notes' => [17, 19, 15, 18, 14, 16]
+    ],
+    [
+        'prenom' => 'Lea',
+        'nom' => 'Jane',
+        'notes' => [18, 13, 12, 16, 11, 17]
+    ]
+];
