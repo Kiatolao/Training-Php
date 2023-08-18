3c7dc50bf56a7d48c6c35393fd61b728e0696b3b
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-18 03:07:26 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -1,18 +1,31 @@
 <?php
 
 //Fonction qui affiche la moyenne
-$notes = readline('Quelle est votre note ?: ');
+$notes = (int)readline('Quelle est votre note ?: ');
 
-if ($notes >=10) {
-    if ($notes == 10 ){
-        echo 'Vous avez juste la moyenne';
-    } else {
+if ($notes >10) {
     echo 'Bravo vous avez la moyenne';
-    }
-} else {
+} elseif ($notes === 10) {
+    echo 'Vous avez juste la moyenne';
+}
+ else {
     echo 'Dommage vous n\'avez pas la moyenne';
 }
 
-//fonction ineractive
-
+//Fonction switch
+$action = (int)readline('Entrez votre action: (1: attaquer, 2: défendre, 3: passer mon tour)');
+switch ($action) {
+    case 1:
+        echo 'J\'attaque';
+        break;
+    case 2:
+        echo 'Je défends';
+        break;
+    case 3:
+        echo 'Je passe mon tour';
+        break;
+    default:
+        echo 'Commande inconnue';
+        break;
+}
 
