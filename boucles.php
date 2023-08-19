c39a788ee058e472c822cb4bf038ac384c5fa3af
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-19 03:01:38 +0200

    Update boucles.php

--- boucles.php
+++ boucles.php
@@ -52,5 +52,43 @@ foreach ($eleves as $classe => $listEleves) {
     foreach ($listEleves as $eleve) {
         echo "- $eleve\n";
     }
-    echo "";
+    echo "\n";
+}
+
+
+//Exercice foreach
+
+/*
+Demander à l utilisateur de taper une note ou "fin" pour terminer
+Creer un tableau pour rentrez les notes (penser à $note[])
+A la fin afficher la liste de notes
+*/
+
+/*
+Tant que l utilisateur ne tape pas "fin"
+    On ajoute la note tapée dans un tableau
+Pour chaque note dans notes
+    on affiche la note
+*/
+
+/* $notes = [];
+
+while (true) {
+    $action = readline("Entrez une note ou taper 'fin' pour terminer : ");
+    if ($action === 'fin'){
+        break;
+    } else {
+        $notes[] = (int)$action;
+    }
+};
+
+foreach ($notes as $note) {
+    echo "- $note \n";
+} */
+
+$horaires = [];
+$jours = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
+
+for ($i = 0; $i < 5; $i++) {
+    $action = readline("Entrez l'horaire pour $jours[$i] ");
 }
