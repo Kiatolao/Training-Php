ebbe42617c28a75d97448b18a8a80f8966a2a401
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-20 21:33:37 +0200

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -1,12 +1,31 @@
 <?php
 
 // Fonction invert
-$action = (string)readline('Entrez un mot pour vérifier si c\'est un palindrome : ');
+/* $action = readline('Entrez un mot pour vérifier si c\'est un palindrome : ');
 
 $inverse = strtolower(strrev($action));
 if (strtolower($action) === $inverse) {
     echo $action . ' est palindrome';
 } else {
     echo $action . ' n\'est pas palindrome ';
-}
-?>
+} */
+
+
+//Fonction somme/moyenne
+
+/* $notes = [17, 13, 19];
+$moyenne = round(array_sum($notes) / count($notes), 2);
+echo"La moyenne sera de : $moyenne"; */
+
+/*
+Attention aux variables par reference
+Ici a variable &$notes (fonction mutable) sera aussi modifiée contrainement à $notes
+*/
+
+/* $notes  = [14, 20, 19];
+$notes2 = &$notes;
+$notes[] = 11;
+var_dump($notes, $notes2);
+ */
+
+ //Fonction exit
