9d056a9f74a34e03931ea7433e5a1b9184b3d11b
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-21 03:02:41 +0200

    Update fonctionsUser.php

--- fonctionsUser.php
+++ fonctionsUser.php
@@ -9,9 +9,20 @@ bonjour ("Jean \n");
 bonjour ('Marie-Thérèse'); */
 
 //meme fonctions stockée
-function bonjour ($name) {
+/* function bonjour ($name) {
     return "Bonjour $name";
 }
 
 $salutation = bonjour ("Jean \n");
-echo $salutation;
+echo $salutation; */
+
+// alternative
+function bonjour ($name = null) {
+    if ($name === null) {
+        return "Bonjour \n";
+    }
+    return 'Bonjour' . $name ."\n";
+}
+
+echo bonjour ();
+echo bonjour (' Jean');
