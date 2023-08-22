ee5b0a28ddd1590ed647fa10fc671291b646944c
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-22 17:45:15 +0200

    ecriture

---
+++ cli/newsletter.php
@@ -0,0 +1,14 @@
+<?php
+if (!empty($_POST['email'])){
+
+}
+?>
+
+<h1> Test</h1>
+<form action= "/newsletter.php" method="post" >
+    <div>
+        <p>La newsletter est INCROYABLE</p>
+        <input type="email" placeholder="Entrez votre email">
+        <button>S'inscrire</button>
+    </div>
+</form>
