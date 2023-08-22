3e7dff79896fd7ed7d79eddf980ace02a39ddf2e
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-22 04:31:32 +0200

    Update lecture.php

--- lecture.php
+++ lecture.php
@@ -6,5 +6,6 @@ pour executer la commande
 php lecture.php
 */
 
-// connaitre le chemin d'un path
+// connaitre le chemin d'un path et le placer dans un dir spécifique
 var_dump(__DIR__);
+$fichier = __DIR__. DIRECTORY_SEPARATOR . '\\C:\Users\kiato\Documents\0- Formation Web developper full stack\PHP\training\Training-Php\lecture';
