dd92bee551ad3850c8d4970784eda46072a0324c
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-22 04:44:37 +0200

    Update lecture.php

--- cli/lecture.php
+++ cli/lecture.php
@@ -1,10 +1,20 @@
 <?php
 
-// connaitre le chemin d'un path et le placer dans un dir spécifique
+/*
+ connaitre le chemin d'un path et le placer dans un dir spécifique
+ pour remonter un dossier 
+ var_dump (dirname(__DIR__,x));
+ */
 var_dump(__DIR__);
 $fichier = __DIR__. DIRECTORY_SEPARATOR . 'demo.txt';
 
-file_put_contents ($fichier,'Salut!');
+$size= file_put_contents ($fichier,'Salut!');
+if($size === false){
+    echo'Impossible d\'ecrire le fichier';
+} else {
+    echo 'Ecriture reussie';
+}
+
 /* 
 pour executer la commande 
 php cli/lecture.php
