7d79f966d5d2216730252354e25c3c6a2b7ccded
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-22 05:19:43 +0200

    updated file for testing

--- cli/lecture.php
+++ cli/lecture.php
@@ -6,7 +6,7 @@
  var_dump (dirname(__DIR__,x));
  */
 var_dump(__DIR__);
-$fichier = __DIR__. DIRECTORY_SEPARATOR . 'demo.txt';
+$fichier = __DIR__. DIRECTORY_SEPARATOR . 'demo.csv';
 
 $size= @file_put_contents ($fichier,"\nSalut");
 if($size === false){
