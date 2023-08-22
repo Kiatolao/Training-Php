9fc153dfb98d1b093e63c998952f9ae4988f0dc9
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-22 16:05:56 +0200

    testing reading

--- cli/menu.php
+++ cli/menu.php
@@ -1,9 +1,22 @@
 <?php
-$menu = __DIR__ . DIRECTORY_SEPARATOR . 'menu.txt';
-$menuList = file_get_contents($menu);
-echo $menuList;
+$menu = __DIR__ . DIRECTORY_SEPARATOR . 'menu.csv';
+$lignes = file($menu);
+foreach ($lignes as $k => $ligne) {
+    $lignes[$k]= explode("\t", trim($ligne));
+}
 ?>
 
 <h1> Liste des menus</h1>
+
+<?php foreach ($lignes  as $ligne) : ?>
+    <?php if (count($ligne) === 1) : ?>
+        <h2> <?= $ligne[0]; ?></h2></br>
+    <?php else : ?>
+        <p> <?= $ligne[0]; ?></br>
+        <?= $ligne[1]; ?>
+        </p>   
+    <?php endif; ?>
+<?php endforeach; ?>    
+   
 
 
