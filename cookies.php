222deb633cd3afa720efcd24344f3af6ad93b76d
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-23 20:30:15 +0200

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -1,4 +1,13 @@
 <?php
 // basic cookie line 
 setcookie('utilisateur', 'jhon', time() + 60*60*24);
+var_dump ($_COOKIE);
 
+?>
+
+<form action="" method="post">
+    <div>
+        <input type="text" name="champs" placeholder="Entrez votre nom">
+        <button>Envoyer</button>
+    </div>
+</form>
