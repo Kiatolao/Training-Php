29f87be12908fa4205f5aecb90455f8cf0accfb6
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-24 20:28:59 +0200

    Update jeu.php

--- jeu.php
+++ jeu.php
@@ -5,19 +5,20 @@ $deviner =150;
 
 <!-- recupère le nombre -->
 <pre>
-    <?php var_dump($_GET['chiffre']) ?>
+    <?php var_dump($_GET) ?>
 </pre>
 
 <!-- input user -->
-<?php if ($_GET['chiffre'] > $deviner): ?>
-    <p>Votre chiffre est trop grand</p>
-<?php elseif ($_GET['chiffre'] < $deviner): ?>
-    <p>Votre chiffre est trop petit</p>
-<?php else :?>
-    <p>Bravo! Vous avez trouvé le bon chiffre!</p>
+<?php if (isset($_GET['chiffre'])) :?>
+    <?php if ($_GET['chiffre'] > $deviner): ?>
+        <p>Votre chiffre est trop grand</p>
+    <?php elseif ($_GET['chiffre'] < $deviner): ?>
+        <p>Votre chiffre est trop petit</p>
+    <?php else :?>
+        <p>Bravo! Vous avez trouvé le bon chiffre!</p>
+    <?php endif; ?>
 <?php endif; ?>
-
-<form action="jeu.php" method="GET">
-    <input type="number" name="chiffre" placeholder="Entrez un numbre entre 0 et 1000" value="<?= htmlentities($_GET['chiffre'])?>">
+<form action="/jeu.php" method="GET">
+    <input type="number" name="chiffre" placeholder="Entrez un numbre entre 0 et 1000" value="<?php if (isset($_GET['chiffre'])) {echo htmlentities($_GET['chiffre']);} ?>">
     <button type="submit">Submit</button>
 </form>
