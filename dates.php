bab4ffa6f94dac16035a5e33c4888b5ea864a76e
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-25 19:45:54 +0200

    Update dates.php

--- dates.php
+++ dates.php
@@ -3,3 +3,22 @@ define ('CRENEAUX', [
     [8, 12],
     [14, 19]
 ]);
+CRENEAUX;
+function creneaux_html (array $creneaux {
+
+});
+$creneaux = creneaux_html(CRENEAUX);
+?>
+
+
+
+
+<div>
+    <div>
+    <h1>Horaires</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officia, odit, minima iure culpa qui dolorum consectetur quod ducimus perspiciatis est voluptates quos, officiis rem!</p>
+        <div>
+            Le magasin sera ouvert de à
+        </div>
+    </div>
+</div>
